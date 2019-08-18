f4a49e541977bdc7c9441ea3c67a692cf50c6cc4
Ajay <ajayau404@gmail.com>
2019-08-18 13:11:04 +0530

    sniffer

--- src/raw_sniff.py
+++ src/raw_sniff.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 import sys, os, time, json
+import scapy.all as scapy
+
+def processPkt(inpPacket):
+	inpPacket.show()
 
 def main():
-
+	## sniffer on interface
+	scapy.sniff(iface="wlp3s0", prn=processPkt)#, filter="tcp", store=0)
 
 if __name__ == "__main__":
 	main()
